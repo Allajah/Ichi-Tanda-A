1cefc4691a91b363558da1b60b206a21b1290c49
Ryoma ABE <arayahomuhomu@gmail.com>
2014-06-29 16:46:36 +0900

    インデント揃えた

--- Ruby/loop.rb
+++ Ruby/loop.rb
@@ -3,14 +3,14 @@ require 'yaml'
 alphabets=YAML.load(File.open('./alphabet.yml'))
 num=1
 loop do
-   unless(num==1000)
-  print num.to_s+"反田"+alphabets[num%26-1] + "\n"
-   else
-       print "千反田"+alphabets[num%26-1]+"\n"
-   end
+    unless(num==1000)
+        print num.to_s+"反田"+alphabets[num%26-1] + "\n"
+    else
+        print "千反田"+alphabets[num%26-1]+"\n"
+    end
 
-  num+=1
-  if num==1001
-      break 
-  end
+    num+=1
+    if num==1001
+        break 
+    end
 end
